eb2b70cd7aeae1290918552f8ebb12d3c22ce254
Youtmax <youtmax654@gmail.com>
2024-01-28 23:15:47 +0100

    reshaping the quote machine

--- app/page.tsx
+++ app/page.tsx
@@ -32,33 +32,48 @@ export default function Home() {
       className="h-full duration-[2s] flex justify-center items-center"
       style={{ backgroundColor: color }}
     >
-      <div id="quote-box" className="bg-white w-[500px] rounded-xl">
-        <FaQuoteLeft className="duration-[2s]" size={28} color={color} />
-        <h1
-          id="text"
-          className="text-4xl duration-[2s]"
-          style={{ color: color }}
-        >
-          {quoteContent}
-        </h1>
+      <div
+        id="quote-box"
+        className="bg-white max-w-[600px] rounded-xl flex flex-col p-4"
+      >
+        <div className="flex flex-row">
+          <FaQuoteLeft className="duration-[2s]" size={28} color={color} />
+          <h1
+            id="text"
+            className="text-xl duration-[2s] ml-2"
+            style={{ color: color }}
+          >
+            {quoteContent}
+          </h1>
+        </div>
         <h2
           id="author"
-          className="text-2xl duration-[2s]"
+          className="duration-[2s] flex flex-row-reverse"
           style={{ color: color }}
         >
           - {author}
         </h2>
-        <button
-          onClick={() => getQuote()}
-          id="new-quote"
-          className="text-white p-2 rounded-md duration-[2s]"
-          style={{ backgroundColor: color }}
-        >
-          New Quote
-        </button>
-        <a id="tweet-quote" href="https://twitter.com/intent/tweet">
-          <FaSquareXTwitter className="duration-[2s]" size={28} color={color} />
-        </a>
+        <div className="flex flex-row-reverse justify-between mt-4">
+          <button
+            onClick={() => getQuote()}
+            id="new-quote"
+            className="text-white p-2 rounded-md duration-[2s] max-w-fit"
+            style={{ backgroundColor: color }}
+          >
+            New Quote
+          </button>
+          <a
+            id="tweet-quote"
+            href="https://twitter.com/intent/tweet"
+            className="max-w-fit block"
+          >
+            <FaSquareXTwitter
+              className="duration-[2s]"
+              size={28}
+              color={color}
+            />
+          </a>
+        </div>
       </div>
     </div>
   );
